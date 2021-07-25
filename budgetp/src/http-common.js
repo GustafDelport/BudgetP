83d7390b21aaa3b9245d8e5332e9da3e736d0b5a
Gustaf Delport <gustafdelport@gmail.com>
2021-07-25 21:33:14 +0200

    Added React Dom

--- budgetp/src/http-common.js
+++ budgetp/src/http-common.js
@@ -6,24 +6,3 @@ export default axios.create({
         "Content-type" : "application/json"
     }
 });
-
-// const api = axios.create({
-//     baseURL: 'http://localhost:3000/',
-//     headers: {
-//         "Content-type" : "application/json"
-//     }
-// })
-
-// export const UpdateBudget = (id, payload) => api.put(`/UpdateBudget/:${id}`,payload)
-// export const GetBudget = () => api.get('/budgetAll')
-// export const GetExpenses = () => api.get('/expensesAll')
-// export const DeleteExpense = (id) => api.get(`/DeleteExpense/:${id}`)
-
-// const apis = {
-//     UpdateBudget,
-//     GetBudget,
-//     GetExpenses,
-//     DeleteExpense,
-// }
-
-// export default apis;
